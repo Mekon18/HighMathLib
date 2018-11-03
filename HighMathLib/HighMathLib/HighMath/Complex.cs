8b522c2c424ca4553a379a566929233b2b919440
Mekon18 <maksim.grummo@mail.ru>
2018-11-03 12:24:14 +0300

    Add files via upload

---
+++ HighMathLib/HighMathLib/HighMath/Complex.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace HighMath
+{
+    public struct Complex
+    {
+        public double Imaginary { get; set; }
+        public double Real { get; set; }
+
+        public Complex(double a, double b)
+        {
+            Real = a;
+            Imaginary = b;
+            
+        }
+
+        public override string ToString()
+        {
+            if (double.IsNaN(Real) || double.IsNaN(Imaginary))
+            {
+                return "NaN";
+            }
+            if (Real == 0 && Imaginary == 0)
+            {
+                return "0";
+            }
+            string str = "";
+            if (Real != 0)
+            {
+                str += Real;
+            }
+            if (Imaginary != 0)
+                if (Real != 0)
+                {
+                    if (Math.Sign(Imaginary) == -1)
+                    {
+                        str += $" - {Math.Abs(Imaginary)}i";
+                    }
+                    else
+                    {
+                        str += $" + {Imaginary}i";
+                    }
+                }
+                else
+                {
+                    str += Imaginary + "i";
+                }
+            return str;
+        }
+
+        public static Complex Parse(string str)
+        {
+            double r = 0;
+            double i = 0;
+            var parts = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            switch (parts.Length)
+            {
+                case 1:
+
+                    if (parts[0].IndexOf('i') != -1)
+                    {
+                        i = double.Parse(parts[0].Remove(parts[0].Length - 1, 1));
+                    }
+                    else
+                    {
+                        r = double.Parse(parts[0]);
+                    }
+                    break;
+                case 3:
+                    r = double.Parse(parts[0]);
+                    i = double.Parse(parts[2].Remove(parts[2].Length - 1, 1));
+                    if (parts[1] == "-")
+                        i *= -1;
+                    break;
+                default:
+                    throw new FormatException();
+            }
+            return new Complex(r, i);
+        }
+
+        public double Abs()
+        {
+            return Math.Sqrt(Real * Real + Imaginary * Imaginary);
+        }
+
+        public static Complex operator +(double d, Complex c)
+        {
+            Complex res = new Complex(c.Real + d, c.Imaginary);
+            return res;
+        }
+        public static Complex operator +(Complex c, double d)
+        {
+            Complex res = d + c;
+            return res;
+        }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex res = new Complex(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
+            return res;
+        }
+
+        public static Complex operator -(double d, Complex c)
+        {
+            Complex res = new Complex(d - c.Real, c.Imaginary);
+            return res;
+        }
+        public static Complex operator -(Complex c, double d)
+        {
+            Complex res = new Complex(c.Real - d, c.Imaginary);
+            return res;
+        }
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            Complex res = new Complex(c1.Real - c2.Real, c1.Imaginary - c2.Imaginary);
+            return res;
+        }
+        public static Complex operator -(Complex c)
+        {
+            return new Complex(-c.Real, -c.Imaginary);
+        }
+
+        public static Complex operator *(Complex c1, Complex c2)
+        {
+            Complex res = new Complex(c1.Real * c2.Real - c1.Imaginary * c2.Imaginary, c1.Real * c2.Imaginary + c1.Imaginary * c2.Real);
+            return res;
+        }
+        public static Complex operator *(double d, Complex c)
+        {
+            Complex res = new Complex(d, 0) * c;
+            return res;
+        }
+        public static Complex operator *(Complex c, double d)
+        {
+            Complex res = c * new Complex(d, 0);
+            return res;
+        }
+
+        public static Complex operator /(Complex c1, Complex c2)
+        {
+            Complex res = new Complex((c1.Real * c2.Real + c1.Imaginary * c2.Imaginary) / (c2.Real * c2.Real + c2.Imaginary * c2.Imaginary),
+                (c2.Real * c1.Imaginary - c2.Imaginary * c1.Real) / (c2.Real * c2.Real + c2.Imaginary * c2.Imaginary));
+            return res;
+        }
+        public static Complex operator /(double d, Complex c)
+        {
+            Complex res = new Complex(d, 0) / c;
+            return res;
+        }
+        public static Complex operator /(Complex c, double d)
+        {
+            Complex res = c / new Complex(d, 0);
+            return res;
+        }
+    }
+}
---
+++ HighMathLib/HighMathLib/HighMath/Vector.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace HighMath
+{
+    public struct Vector
+    {
+        private double _x;
+        private double _y;
+        private double _z;
+        public double X { get { return _x; } set { _x = value; GetLength(); } }
+        public double Y { get { return _y; } set { _y = value; GetLength(); } }
+        public double Z { get { return _z; } set { _z = value; GetLength(); } }
+        public double Length { get; private set; }
+
+        public Vector(double x, double y, double z) : this()
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        private void GetLength()
+        {
+            Length = Math.Sqrt(X * X + Y * Y + Z * Z);
+        }
+
+        public override string ToString()
+        {
+            return $"({X}; {Y}; {Z})";
+        }
+
+        #region operators
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        }
+        public static Vector operator *(Vector v, double a)
+        {
+            return new Vector(v.X * a, v.Y * a, v.Z * a);
+        }
+        public static Vector operator /(Vector v, double a)
+        {
+            return new Vector(v.X / a, v.Y / a, v.Z / a);
+        }
+        public static Vector operator *(double a, Vector v)
+        {
+            return new Vector(v.X * a, v.Y * a, v.Z * a);
+        }
+        public static Vector operator /(double a, Vector v)
+        {
+            return new Vector(v.X / a, v.Y / a, v.Z / a);
+        }
+        public static Vector operator -(Vector v)
+        {
+            return new Vector(-v.X, -v.Y, -v.Z);
+        }
+        #endregion
+
+        public static Vector Point3DToVector(Point3D p)
+        {
+            Vector V = new Vector(p.X, p.Y, p.Z);
+            return V;
+        }
+
+        public static double ScalarMultiplying(Vector v1, Vector v2)
+        {
+            return v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z;
+        }
+
+        public static double GetCos(Vector v1, Vector v2)
+        {
+            return ScalarMultiplying(v1, v2) / (v1.Length * v2.Length);
+        }
+    }
+}
